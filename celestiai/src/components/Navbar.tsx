e3c673a3203b032918baeda0981587c4ec3ddc71
Dustin Eilers <dustineilers@Dustins-MacBook-Air.local>
2025-09-30 20:21:34 -0400

    Create Navbar

--- celestiai/src/components/Navbar.tsx
+++ celestiai/src/components/Navbar.tsx
@@ -1,1 +1,49 @@
-import React from 'react';
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+
+
+const pages = [
+    { label: "Home", path: "/" },
+    { label: "About", path: "/about" },
+    { label: "Services", path: "/services" },
+    { label: "Contact", path: "/contact" },
+];
+
+const Navbar: React.FC = () => {
+    return (
+        <AppBar position="static" color="primary">
+            <Toolbar>
+                <Typography
+                    variant="h6"
+                    component={Link}
+                    to="/"
+                    sx={{
+                        flexGrow: 1,
+                        color: "inherit",
+                        textDecoration: "none",
+                        fontWeight: 700,
+                        letterSpacing: ".15rem",
+                    }}
+                >
+                    CelestiaAI
+                </Typography>
+
+                <Box>
+                    {pages.map((page) => (
+                        <Button
+                            key={page.path}
+                            component={Link}
+                            to={page.path}
+                            sx={{ color: "white" }}
+                        >
+                            {page.label}
+                        </Button>
+                    ))}
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Navbar;
---
+++ celestiai/src/pages/Services.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Services: React.FC = () => {
+    return (
+        <></>
+    )
+};
+
+export default Services;
